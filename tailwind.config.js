d9c66b7566f235c07d85f750a787c247c5cb3775
marvin-santos-sawce <contato.marvin19@gmail.com>
2026-05-21 11:32:04 -0300

    Instação do tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./faq.html",
+    "./js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
